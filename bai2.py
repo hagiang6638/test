0b0dc18b68c85b94f2097f62c1079e0cbcf81e2a
hagiang6638 <hagiang6638@gmail.com>
2022-02-04 17:06:46 +0700

    Test

---
+++ bai2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 14:34:06 2022
+
+@author: HP
+"""
+
+x = int(input("Nhập x: ")) 
+ketQua = x**3 + 3*(x**2) + x + 1
+print("Kết quả của biểu thức A(x) là ",ketQua)
